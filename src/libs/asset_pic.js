74d04dd811aa3da66ab38d741e80eed547d9a93f
helloabt <2439897034@qq.com>
2019-10-08 11:42:13 +0800

    1.Add some missing files;2.Optimize the index and paement page display

---
+++ src/libs/asset_pic.js
@@ -0,0 +1,22 @@
+//picture asset
+//small blur size: 25 * 25
+//asset_did&link: sha1sum <picture file>
+//hd_src: date |sha1sum
+module.exports = [
+  {
+    category: 'animal',
+    owner: 'Robert',
+    contact: 'wx:mave99a',
+    owner_did: 'did:abt:z1XKKV5TykX467ZXNo3F168Raq6eTF8tWGR',
+    blur_src: '/static/images/payment/animal/robert/bark.jpg',
+    hd_src: '/static/images/payment/animal/robert/bark/f9c86d8761cc58a906f3c64e9e6aea14576e3b84.jpg',
+    asset_did: 'aa253d712befd40bd0200378086485acb23231f1',
+    link: '/payment?memo=aa253d712befd40bd0200378086485acb23231f1',
+    title: '老冒的巴克',
+    description: '',
+    worth: '28',
+    token_sym: 'TBA',
+    payback_rate: '0.6',
+  },
+];
+
